79af0beffbf3de66a3eb2ca799a02629a233f05c
Rohit Gupta <49370602+rgrohitgupta938@users.noreply.github.com>
2024-09-03 16:08:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
 //your JS code here. If required.
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currActive = 1;
+
+next.addEventListener("click", () => {
+  currActive++;
+  if (currActive > circles.length) {
+    currActive = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currActive--;
+  if (currActive < 1) {
+    currActive = 1;
+  }
+  update();
+});
+
+const update = () => {
+  circles.forEach((cr, inx) => {
+    if (inx < currActive) {
+      cr.classList.add("active");
+    } else {
+      cr.classList.remove("active");
+    }
+  });
+  const actives = document.querySelectorAll(".active");
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currActive === 1) {
+    prev.disabled = true;
+  } else if (currActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+};
